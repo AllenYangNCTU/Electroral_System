129e612f5b5447b5f0e6de7b1915494c5502e934
Kamigami85 <yanghejie.bt04@nycu.edu.tw>
2022-07-05 14:37:51 +0800

    會員修改密碼成功會員修改密碼成功

--- back/update_pwd.php
+++ back/update_pwd.php
@@ -1,4 +1,17 @@
 <?php
 include_once("../api/base.php");
 
-dd($_GET);
+?>
+<h1 style="text-align: center">密碼驗證</h1>
+<form action="./update_pwd_check.php" method="post">
+    <p>使用者名稱</p>
+    <input type="text" name="name" value="<?= $_GET['account']; ?>" id="" readonly>
+    <p>舊密碼</p>
+    <input type="password" name="oldpwd" id="">
+    <p>新密碼</p>
+    <input type="password" name="pwd" id="">
+    <p>再次輸入新密碼</p>
+    <input type="password" name="re_pwd" id=""><br>
+    <button type="submit">送出</button>
+</form>
+<!-- <form action="" method="post"></form> -->
---
+++ back/update_pwd_check.php
@@ -0,0 +1,33 @@
+<?php
+include_once("../api/base.php");
+
+$sql = "select pw from `users` where `acc`='{$_POST['name']}'";
+$md5pwd = $pdo->query($sql)->fetch(PDO::FETCH_ASSOC); //導出資料
+
+if ((md5($_POST['oldpwd']) == $md5pwd['pw']) && ($_POST['pwd'] == $_POST['re_pwd'])) {
+
+    $newpwd = md5($_POST['pwd']);
+    $sql = "UPDATE `users`
+    SET    `pw`= '{$newpwd}'
+    WHERE  `acc`='{$_POST['name']}'";
+    $pdo->exec($sql);
+    print("密碼修改成功");
+?>
+    <a href="../login.php">重新登入</a>
+<?php
+} else if (md5($_POST['oldpwd']) != $md5pwd['pw']) {
+    print("舊密碼錯誤");
+?>
+    <a href="./update_pwd.php">返回</a>
+<?php
+} else if ($_POST['pwd'] != $_POST['re_pwd']) {
+    print("兩次輸入的密碼不同");
+?>
+    <a href="./update_pwd.php">返回</a>
+<?php
+} else {
+    print("error!");
+?>
+    <a href="./update_pwd.php">返回</a>
+<?php
+}
