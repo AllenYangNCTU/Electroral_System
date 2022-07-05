b4a6c82746d4ada9214e1774732c60256485ec27
Kamigami85 <yanghejie.bt04@nycu.edu.tw>
2022-07-05 11:00:54 +0800

    忘記密碼驗證以及更新功能忘記密碼驗證以及更新功能

---
+++ back/enternewpwd.php
@@ -0,0 +1,26 @@
+<?php
+include_once("../api/base.php");
+// dd($_POST);
+if ($_POST['pwd'] == $_POST['re_pwd']) {
+    // print($_POST['pwd']);
+    // print($_POST['username']);
+    // print(md5($_POST['pwd']));
+    $newpwd = md5($_POST['pwd']);
+    $sql = "UPDATE `users`
+    SET    `pw`= '{$newpwd}'
+    WHERE  `acc`='{$_POST['username']}'";
+
+
+    // print($sql);
+    $pdo->exec($sql);
+    print("密碼重設成功");
+?>
+    <br><a href="../login.php">返回登入</a>
+<?php
+} else {
+    print("密碼前後不一致");
+?>
+    <br><a href="./resetpwd.php">返回</a>
+<?php
+}
+?>
--- back/resetpwd.php
+++ back/resetpwd.php
@@ -2,23 +2,22 @@
 include_once "../api/base.php"; //連線資料庫
 // dd($_GET);
 ?>
-<form action="resetpwd.php" method="post">
+<h1 style="text-align:center;">重設密碼</h1>
+<form action="enternewpwd.php" method="post">
+    <p>username</p>
+    <input type="text" name="username" value="" id="">
     <p>pwd</p><br>
-    <input type="password" name="pwd" value="pwd" placeholder="password" id="">
+    <input type="password" name="pwd" value="" id="">
     <p>repwd</p><br>
-    <input type="password" name="re_pwd" value="re_pwd" placeholder="reenter password" id=""">
+    <input type="password" name="re_pwd" value="" id="""><br>
+    <button type=" submit" value="submit" onclick="location.href='./enternewpwd.php'">送出</button>
 </form>
 <?php
-// if ($_GET['action'] == 'upgrade') {
-//     $sql = "UPDATE `users` -- 更新資料表
-//     SET    `admin`= 1
-//     WHERE  `id`='{$_GET['id']}'";
-// } else {
-//     $sql = "UPDATE `users` -- 更新資料表
-//     SET    `admin`= 0
-//     WHERE  `id`='{$_GET['id']}'";
+// header_to("./enternewpwd.php");
+// if (isset($_POST) && ($_POST['pwd'] == $_POST['re_pwd'])) {
+//     print('hello');
 // }
 
-// $pdo->exec($sql);
 
 // header_to("../member_managements.php");
+?>
