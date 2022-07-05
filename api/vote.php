3f43f38776dcd8b81269b69fc0e2ee49acb362f1
Kamigami85 <yanghejie.bt04@nycu.edu.tw>
2022-07-05 21:42:02 +0800

    修復投票功能修復投票功能

--- api/vote.php
+++ api/vote.php
@@ -6,7 +6,7 @@ if (isset($_POST['opt'])) {
 
   if (is_array($_POST['opt'])) {
     foreach ($_POST['opt'] as $key => $opt) {
-      $option = find("options", $opt);
+      $option = find_something_in_table("options", $opt);
       $option['total']++;
       update_or_insert_contents_in_table("options", $option);
       if ($key == 0) {
@@ -14,12 +14,12 @@ if (isset($_POST['opt'])) {
         $subject['total']++;
         update_or_insert_contents_in_table("subjects", $subject);
       }
-      $log = [
-        'user_id' => (isset($_SESSION['user'])) ? $_SESSION['user'] : 0,
-        'subject_id' => $subject['id'],
-        'option_id' => $option['id']
-      ];
-      update_or_insert_contents_in_table("logs", $log);
+      // $log = [
+      //   'user_id' => (isset($_SESSION['user'])) ? $_SESSION['user'] : 0,
+      //   'subject_id' => $subject['id'],
+      //   'option_id' => $option['id']
+      // ];
+      // update_or_insert_contents_in_table("logs", $log);
     }
   } else {
     //單選題
@@ -29,12 +29,12 @@ if (isset($_POST['opt'])) {
     $subject = find_something_in_table("subjects", $option['subject_id']);
     $subject['total']++;
     update_or_insert_contents_in_table("subjects", $subject);
-    $log = [
-      'user_id' => (isset($_SESSION['user'])) ? $_SESSION['user'] : 0,
-      'subject_id' => $subject['id'],
-      'option_id' => $option['id']
-    ];
-    update_or_insert_contents_in_table("logs", $log);
+    // $log = [
+    //   'user_id' => (isset($_SESSION['user'])) ? $_SESSION['user'] : 0,
+    //   'subject_id' => $subject['id'],
+    //   'option_id' => $option['id']
+    // ];
+    // update_or_insert_contents_in_table("logs", $log);
   }
 }
 header_to("../index.php?do=vote_result&id={$option['subject_id']}");
