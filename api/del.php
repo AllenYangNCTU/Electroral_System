ac03cd64fc4bb21afb69c5beb7335d0685433485
Kamigami85 <yanghejie.bt04@nycu.edu.tw>
2022-07-13 13:27:16 +0800

    解決刪除投票主題沒刪掉logs的問題

--- api/del.php
+++ api/del.php
@@ -1,6 +1,19 @@
 <?php
 include_once "base.php";
 
+
+
+$sql_delete_logs = "delete from logs where subject_id = {$_GET['id']}";
+$pdo->exec($sql_delete_logs);
+
+
+
+
+
+
+
+
+
 $table = $_GET['table'];
 $id = $_GET['id'];
 if ($table == 'subjects') {
